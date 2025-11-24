2843440b8347814a058eb46713b00d57895f2c73
rukataka0505 <rukataka0505@gmail.com>
2025-11-24 13:39:40 +0900

    backup: 2025-11-24 13:39:40

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Next.js 16以降、Turbopackの設定はトップレベルの 'turbopack' キーに記述します
   turbopack: {
     root: __dirname,
-  }
+  },
 };
 
 export default nextConfig;
